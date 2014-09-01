03d676c5670be1d7bcd8b22841160dea0b8435d6
jmontilla012 <jmontilla012@hotmail.com>
2014-08-31 20:19:10 -0400

    Update

--- django-initialize.bash
+++ django-initialize.bash
@@ -21,8 +21,7 @@ source .env/bin/activate
 echo "================================="
 echo "= Installing django==$djversion ="
 echo "================================="
-if [ $djversion == "1.7" ]; 
-    then
+if [ $djversion == "1.7" ];then
 	pip install https://www.djangoproject.com/download/1.7c3/tarball/
 else
     pip install django==$djversion
@@ -30,5 +29,19 @@ fi
 django-admin.py startproject $projectname
 cd $projectname/$projectname
 mkdir  static/ media/ apps/ templates/ apis/
+rm settings.py urls.py wsgi.py
+if [[ $djversion=="1.4" ]]; then
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.4/settings.py 
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.4/urls.py
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.4/wsgi.py 
+elif [[ $djversion=="1.5" ]];then    
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.5/settings.py 
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.5/urls.py
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.5/wsgi.py 
+elif [[ $djversio=="1.6" ]];then   
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.6/settings.py 
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.6/urls.py
+    wget  http://conf.kitdevelop.com/etc/django/settings/django-1.6/wsgi.py 
+fi    
 touch static/__init__.py media/__init__.py  apps/__init__.py templates/__init__.py  apis/__init__.py
 echo "{$projectname, DONE}."
