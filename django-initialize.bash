47cba40f426ab290fae624fc386515faa5bc4d49
jmontilla012 <jmontilla012@hotmail.com>
2014-10-07 03:18:50 -0400

    modified django-initialize.bash

--- django-initialize.bash
+++ django-initialize.bash
@@ -1,6 +1,8 @@
 #!bin/bash
 #by Junior Montilla
 
+TEMPLATE14=$HOME/.django-templates/django-1.4-conf-templates.zip
+
 reset
 
 echo 
@@ -15,7 +17,13 @@ echo "Django version? (1.4, 1.5, 1.6)"
 read djversion
 
 mkdir -p $HOME/git/django/$projectname/
-mkdir  $HOME/.django-templates/
+mkdir -p  $HOME/.django-templates/
+if [ -e "$TEMPLATE14"  ];then       
+    echo "The file exists."
+else
+    wget  -q --no-check-certificate https://s3-us-west-2.amazonaws.com/conf.django-initialize/django-templates/1.4/django-1.4-conf-templates.zip -O $TEMPLATE14
+fi
+
 cd $HOME/git/django/$projectname/
 
 echo 
@@ -27,12 +35,12 @@ virtualenv .env
 source .env/bin/activate
 
 echo 
-echo "* Installing django and starting project *"
+echo "* Installing django *"
 echo 
 
 if [ $djversion == "1.4" ];then
         pip install django==1.4.5
-	django-admin.py startproject --template=file://$HOME/.django-templates/django-1.4-conf-templates.zip $projectname
+	django-admin.py startproject --template=file://$TEMPLATE14 $projectname
 fi	
 
 cd  $projectname/
@@ -40,7 +48,7 @@ cd  $projectname/
 echo
 echo "* initialize project as git repo *"
 echo
-wget  -q https://s3-us-west-2.amazonaws.com/conf.django-initialize/.gitignore
+wget  -q --no-check-certificate https://s3-us-west-2.amazonaws.com/conf.django-initialize/.gitignore
 git init
 
 echo 
